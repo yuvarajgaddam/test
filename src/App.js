6ff98a817da68fd88bc9c30741f19cf27b1b9df9
yuvaraj <yuvaraju.github@gmail.com>
2024-09-02 00:55:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hey there page under maintanance. Please try again after few days.
+    </div>
+  );
+}
+
+export default App;
